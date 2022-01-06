27c6531453c4ed582c378326199a7cb52646bb9e
unknown <derek.slauson5@gmail.com>
2022-01-06 10:03:57 -0600

    Finished activity

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import {useState} from "react"
 
 function App() {
+  let [counter, changeCounter] = useState(1)
+  window.changeCounter = changeCounter
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{counter}</h1>
+        <h2>{counter}</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
